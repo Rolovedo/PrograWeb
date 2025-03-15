ebe071c03ede8870dc5a7c2cf1dddba7874c83d6
Rolovedo <acevedo314848@gmail.com>
2025-03-14 21:52:58 -0500

    CRUD users terminado, falta iniciar el CRUD de proyectos en controller y services

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,104 @@
-const userSErvice = require('../services/user.services');
+const userService = require('../services/user.services');
 
 exports.createUser = async (req, res) => {
     try {
+        
         //Hacemos la primera validacion
         const { nombre, email, password, rol_id, administrador_id } = req.body;
+        
         //Se crea el usuario con los campos que hemos pedido
-        const newUser = await userSErvice.createUser(nombre, email, password, rol_id, administrador_id);
+        const newUser = await userService.createUser(nombre, email, password, rol_id, administrador_id);
+        
         //Mostramos la creacion con status 201 que muestra usuario creado con exito mostrando el usuario creado
         res.status(201).json({ message: 'Usuario creado con exito', user: newUser });
     } catch (err) {
-        res.status(500).json({ message: err.message }); //Muestra el estado de error 500 en caso de no crear el usuario
+
+        //Muestra el estado de error 500 en caso de no crear el usuario
+        res.status(500).json({ message: err.message });
     }
 };
 
+//Relaciona los usuarios relacionados a un administrador especifico
+exports.getAllUsersByAdministradorId = async (req, res) => {
+    try {
+        const admin_from_token = req.user.id;
+        const { email } = req.query;
+
+        //Llama a todos los usuarios
+        const users = await userService.getAllUsersByAdministradorId(admin_from_token, email);
+        
+        //Responde al llamado listando a los usuarios que se requieren con un archivo .json
+        res.status(200).json({ message: 'Usuarios consultados con éxito', users });
+    }
+
+    //Al final siempre el manejo de errores si algo paso mal 
+    catch (error) {
+        res.status(500).json({ message: 'Error al obtener los usuarios', error });
+    }
+};
+
+//Se usa el req (Almacenar la iniformaicion que se pide) y el res (La informacion con la que se responde al usuario)
+exports.getAllUsersByRolId = async (req, res) => {
+    try {
+
+        //Utiliza el servicio de userservice para obtener la lista de los usuarios
+        const users = await userService.getAllUsersByRolId(req.params.id);
+
+        //Nos devuelve un mensaje de confirmacion con la lista de los usuarios .json
+        req.status(200).json({ message: 'Usuarios consultados con éxito', users });
+    } 
+
+    //Mensaje de error por si algo sale mal
+    catch (error) {
+        res.status(500).json({ message: 'Error al obtener los usuarios', error });
+    }
+};
+
+//Controla la actualizacion de un usuario en la base de datos
 exports.updateUser = async (req, res) => {
     const { id } = req.params;
+
+    //Extrae los campos del cuerpo de la solicitud con req.body
+    const { nombre, email, rol_id, administrador_id } = req.body;
+
+    //Agrega un usuario a la solicitud con el req y token
+    const admin_from_token = req.user.id;
+    
+    try {
+
+        //Llama al servicio updateservice de userservice pasando los parametros requeridos
+        const user = await userService.updateUser(id, nombre, email, rol_id, administrador_id, admin_from_token);
+        
+        //SE genera una respuesta con status 200 y mensaje personalizado
+        res.status(200).json({ message: 'Usuario actualizado con éxito', user });
+    } 
+    
+    //Validacion de error con status 500 (Error interno del servidor)
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+//Procesa la eliminacion de un usuario verificando el id del eliminado y que lo haga un administrador
+exports.deleteUser = async (req, res) => {
+
+    //Almacena los parametros con el id
+    const { id } = req.params;
+
+    //Verifica que el usuario este autenticado y pueda eliminar usuarios
+    const admin_from_token = req.user.id;
+    
+    try {
+
+        //Llama el metodo deleteuser de userservice pasando el id y el token
+        const result = await userService.deleteUser(id, admin_from_token);
+        
+        //Respuesta con status 200 y almacena en una constante result
+        res.status(200).json(result);
+    } 
+    
+    //Manejo de errores por si hay alguna falla
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,7 +1,7 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt.js');
 
-exports.createUser = async (nombre, ElementInternals, password, rol_id, administrador_id) => {
+exports.createUser = async (nombre, email, password, rol_id, administrador_id) => {
     try {
         const userExists = await User.finOne({ where: { email } });
         if(userExists) {
@@ -22,6 +22,131 @@ exports.createUser = async (nombre, ElementInternals, password, rol_id, administ
         
         return newUser; //Si todo esta correcto retorna el nuevo usuario
     } catch (err) {
-        throw new Error('Error al crear el usuario: ${err.message}');
+        throw new Error(`Error al obtener los usuarios: ${err.message}`);
+    }
+};
+
+//Se usa el async para que sea asincrona y usar el await
+//Se usa para listar los usuarios vinculados al administrador 
+exports.getAllUsersByAdministradorId = async (administrador_id, email) => {
+    try {
+
+        //Se usa como condicion de busqueda en la base de datos
+        const WhereClause = { administrador_id };
+        if (email) {
+            
+            //Hace que la consulta sea masa especifica
+            WhereClause.email = email; //Se busca si el parametro email fue proporcionado
+        }
+        
+        //Se usa una consulta con el modelo user y excluye el password para mantener la seguridad
+        const users = await User.findAll({ where: WhereClause, attrtbutes: { exclude: ['password'] }});
+        return users;
+    } 
+
+    //Se finaliza con el manejo de errores si algo falla en la consulta
+    catch (err) {
+        throw new Error(`Error al obtener los usuarios: ${err.message}`)
+    }
+};
+
+//Se filtran los usuarios por medio del rol_id
+exports.getAllUsersByRolId = async (rol_id) => {
+    try {
+        
+        //Se usa la busqueda por medio del rol_id como filtro excluyendo el password
+        const users = await User.findAll({ where: { rol_id }, attributes: { exclude: ['password'] } });
+        
+        //Retorna todos los usuarios consultados
+        return users;
+    } 
+
+    //Al final se hace el error siempre por si algo sale mal
+    catch (err) {
+        throw new Error(`Error al obtener los usuarios: ${err.message}`);
+    }
+};
+
+//Actualiza el usuario con todos los parametros que se han ingresado
+exports.updateUser = async (id, nombre, email, rol_id, administrador_id, admin_from_token) => {
+    try {
+
+        //findByPk (Find by primary key) del modelo user que saca el id del usuario
+        const user = await User.findByPk(id);
+
+        //Compara el administrador_id con el token
+        if (user.administrador_id != admin_from_token) {
+
+            //Si los id no coinciden muestra el error a accesso denegado
+            throw new Error('Acceso denegado, este usuario no está bajo su administración');
+        }
+
+        //Verifica si el ususario existe
+        if (!user) {
+
+            //Si no existe saca el error del usuario no encontrado
+            throw new Error('Usuario no encontrado');
+        }
+
+        //Comprueba si el campo email fue proporcionado y si es diferente al email actual del usuario
+        if (email && email !== user.email) {
+
+            //Busca en la base de datos si hay otro usuario con el mismo email
+            const userExists = await User.findOne({ where: { email } });
+            if (userExists) {
+
+                //En caso de encontrar otro usuario lanza este error diciendo que esta en uso el email
+                throw new Error('El email ya está en uso');
+            }
+        }
+
+        //Si no existen errores, actualiza el usuario con la informacion proporcionada
+        await user.update({
+            nombre,
+            email,
+            rol_id,
+            administrador_id
+        });
+
+        return user;
+    } 
+
+    //Si existe error lo lanzamos para el manejo de errores
+    catch (error) {
+        throw new Error(`Error al actualizar usuario: ${error.message}`);
+    }
+};
+
+//Elimina el usuario de la base de datos buscandolo por el id
+exports.deleteUser = async (id, admin_from_token) => {
+    try {
+
+        //Busca el usuario por su id
+        const user = await User.findByPk(id);
+
+        //Compara administrador_id y el token
+        if (user.administrador_id !== admin_from_token) {
+
+            //Si no coinciden saca este error con acceso denegado
+            throw new Error('Acceso denegado, este usuario no está bajo su administración');
+        }
+
+        //Verifica si el ususarios NO existe
+        if (!user) {
+
+            //Lanza el error si el usuario no fue encontrado
+            throw new Error('Usuario no encontrado');
+        }
+
+        //Si pasan los filtros se elimina correctamente el usuario
+        await user.destroy();
+
+        //Arroja un mensaje de eliminacion con exito
+        return { message: 'Usuario eliminado con éxito' };
+    } 
+
+    //En caso de existir error se va al manejo de errores
+    catch (err) {
+        throw new Error(`Error al eliminar el usuario: ${err.message}`);
     }
 };
